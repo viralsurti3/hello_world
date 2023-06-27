e951cd454cb86e7ddf6e826294ec9b5ea26de026
viral surati <viralsurti3@gmail.com>
2023-06-27 16:32:51 +0530

    Leant props

---
+++ src/components/Greetings.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function Greet(){
+//     return <h1>Hello Veeral !</h1>
+// }
+
+// export default Greet
+
+export const Greet = props => <h1>Hello {props.name} from const.</h1>
---
+++ src/components/Welcome.js
@@ -0,0 +1,9 @@
+import React,{Component} from "react";
+
+class Welcome extends Component{
+    render(){
+        return <h3> {this.props.name} Class Component </h3>
+    }
+}
+
+export default Welcome
